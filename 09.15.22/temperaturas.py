81e8f66bc2fd115640aee6855b4ab1f61c7f7f3e
enzoeferreira <enzoeferreira@gmail.com>
2022-09-20 14:26:35 -0300

    [REMOVED] print da lista de temperaturas

--- 09.15.22/temperaturas.py
+++ 09.15.22/temperaturas.py
@@ -4,8 +4,6 @@ temperaturas = []
 for _ in range(quantidade):
     temperaturas.append(float(input("Digite uma temperatura: ")))
 
-print(temperaturas)
-
 print("Temperaturas em ordem crescente:")
 temperaturas.sort()
 print(temperaturas)
