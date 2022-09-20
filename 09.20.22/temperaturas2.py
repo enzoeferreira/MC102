fbdb8b89d0d7a855a7294cf152a4d590c3cd50bc
enzoeferreira <enzoeferreira@gmail.com>
2022-09-20 15:43:37 -0300

    [ADDED] desvio padrão

--- 09.20.22/temperaturas2.py
+++ 09.20.22/temperaturas2.py
@@ -8,6 +8,16 @@ print("Temperaturas em ordem crescente:")
 temperaturas.sort()
 
 print(temperaturas)
-print("Média das temperaturas:", sum(temperaturas)/len(temperaturas))
+media = sum(temperaturas)/len(temperaturas)
+print("Média das temperaturas:", media)
 print("Temperatura Max:", max(temperaturas), "°C")
 print("Temperatura Min:", min(temperaturas), "°C")
+
+# Cálculo do Desvio Padrão Amostral
+n = len(temperaturas)
+soma = 0
+for i in range(len(temperaturas)):
+    soma += (temperaturas[i] - media) ** 2
+desvio_padrao = (soma / (n - 1)) ** 0.5
+
+print("Desvio padrão:", desvio_padrao)
