ed090526476e56dba79ca5b3d88144c1cb9b3cbc
enzoeferreira <enzoeferreira@gmail.com>
2022-09-22 10:15:01 -0300

    [ADDED] salário do funcionário

--- Labs/lab05.py
+++ Labs/lab05.py
@@ -6,32 +6,46 @@
 ##################################################
 
 # Leitura do valor da hora
-V = int(input("Valor da hora de trab: "))
+V = int(input())
 
 # Leitura do numero de dias trabalhados na semana
-D = int(input("Dias de trabalho: "))
+D = int(input())
 
 # Leitora e processamento dos periodos de trabalho de cada dia
 
-horasTrabalhadas = 0
+horasTrabDiaria = []
+horasExtrasDiarias = 0
 for i in range(D):
-    periodo = int(input(f"Periodos do dia {i + 1}: "))
-    print(periodo)
+    horasTrab = 0
+    periodo = int(input())
     
     j = 0
     while j < periodo:
-        start = int(input(f"Começo do periodo {j + 1}: "))
-        stop = int(input(f"Fim do periodo {j + 1}: "))
+        start = int(input())
+        stop = int(input())
         
-        horasTrabalhadas += (stop - start)
+        horasTrab += (stop - start)
         
         j += 1
+    if horasTrab > 8:
+        horasExtrasDiarias += horasTrab - 8
+        print(f"horas extras = {horasExtrasDiarias}")
+    horasTrabDiaria.append(horasTrab)
 
 # Calculo do valor devido ao funcionário
 
+horasExtras = 0
 
+valor = (V * sum(horasTrabDiaria))
+if sum(horasTrabDiaria) > 44:
+    horasExtras += sum(horasTrabDiaria) - 44
+    print(horasExtras)
+    horasExtras += horasExtrasDiarias
+    print(horasExtras)
+if horasExtras > 0:
+    valor += ((V/2) * horasExtras)
 
 # Impressão da saída
-print("Horas trabalhadas:", horasTrabalhadas)
-# print("Horas extras:", horasExtras)
-# print("Valor devido: R$ {:0.2f}".format(valor))
+print("Horas trabalhadas:", sum(horasTrabDiaria))
+print("Horas extras:", horasExtras)
+print("Valor devido: R$ {:0.2f}".format(valor))
