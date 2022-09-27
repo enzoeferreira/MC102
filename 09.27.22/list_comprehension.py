d81a7818fc8c4df90017c6b8d96fa0f8fce13f63
enzoeferreira <enzoeferreira@gmail.com>
2022-09-27 15:40:00 -0300

    [ADDED] list_comprehension.py

---
+++ 09.27.22/list_comprehension.py
@@ -0,0 +1,69 @@
+"""
+Exemplos de formas de escrever listas com list comprehension
+"""
+
+l = []
+
+for i in range(3,10):
+    l.append(i**3)
+
+print(l)
+
+# Equivalente a:
+
+l = [i**3 for i in range(3,10)]
+print(l)
+
+# --------------------------------------
+
+precosOriginais = [1000.0, 550.0, 635.0]
+desconto = 0.3
+
+precosDesconto = []
+for preco in precosOriginais:
+    precosDesconto.append(preco * (1 - desconto))
+print(precosDesconto)
+
+# Equivalente a:
+
+precosOriginais = [1000.0, 550.0, 635.0]
+desconto = 0.3
+
+precosDesconto = [(1 - desconto) * preco for preco in precosOriginais]
+print(precosDesconto)
+
+# --------------------------------------
+
+valores = [6, -1, 8, 0, -5, 6, 2, -2, -3]
+
+valoresNegativos = []
+for valor in valores:
+    if valor < 0:
+        valoresNegativos.append(valor)
+
+print(valoresNegativos)
+
+# Equivalente a:
+
+valores = [6, -1, 8, 0, -5, 6, 2, -2, -3]
+
+valoresNegativos = [n for n in valores if n < 0]
+print(valoresNegativos)
+
+# --------------------------------------
+
+precos = [1000.0, 550.0, 635.0]
+descontos = [0.3, 0.15, 0.2]
+
+precosFinais = []
+for i in range(min(len(precos), len(descontos))):
+    precosFinais.append(precos[i] * (1 - descontos[i]))
+print(precosFinais)
+
+# Equivale a:
+
+precos = [1000.0, 550.0, 635.0]
+descontos = [0.3, 0.15, 0.2]
+
+precosFinais = [precos[i] * (1 - descontos[i]) for i in range(min(len(precos), len(descontos)))]
+print(precosFinais)
